b05c30af33df5e2a70dfd8c0ee75ba3b6b7a5675
Alexandru Ionica <alexandru@ionica.eu>
2018-02-19 23:14:59 +1100

    changed CLI considerably and also added cli commands for validating a config file and dumping the resulted configuration

---
+++ cliargs/cliargs.go
@@ -0,0 +1,89 @@
+package cliargs
+
+import (
+	log "github.com/sirupsen/logrus"
+	"cloudbackup/config"
+	"cloudbackup/daemon"
+	"cloudbackup/misc"
+	"cloudbackup/utils"
+	"sync"
+	"fmt"
+	"os"
+)
+
+const loggingContext = "cliargs"
+
+// top level CLI options and arguments
+type Args struct {
+	Config ArgsCommandConfig `command:"config" description:"configuration file related options"`
+	Start ArgsCommandStart `command:"start" description:"Start the backup daemon"`
+}
+
+type ArgsCommandConfig struct {
+	Validate ArgsCommandConfigCommandValidate `command:"validate" description:"validate provided yaml configuration file"`
+	Dump ArgsCommandConfigCommandDump `command:"dump" description:"dumps the merged configuration. This is a merge of command line arguments, environment variables and then the supplied .yaml config file. Priority is from left to right of the given list. The result will include default values too."`
+}
+
+type ArgsCommandConfigCommandValidate struct {
+	ConfigFile string `short:"c" long:"configfile" description:"Configuration file expected to be in YAML format and have .yml or .yaml extension" required:"true"`
+	Debug bool `short:"d" long:"debug" description:"Set logging to debug in order to see more details about the build up of the configuration"`
+}
+
+type ArgsCommandConfigCommandDump struct {
+	ConfigFile string `short:"c" long:"configfile" description:"Configuration file expected to be in YAML format and have .yml or .yaml extension" required:"true"`
+	Debug bool `short:"d" long:"debug" description:"Set logging to debug in order to see more details about the build up of the configuration"`
+
+}
+
+// arguments for an actual Daemon start
+type ArgsCommandStart struct {
+	Verbose bool `short:"v" long:"verbose" description:"Set logging to verbose"`
+	Debug bool `short:"d" long:"debug" description:"Set logging to debug"`
+	TextLog bool `short:"t" long:"textlog" description:"Set logging to plaintext. Defaults to false which means JSON formatting is used"`
+	ConfigFile string `short:"c" long:"configfile" description:"Configuration file expected to be in YAML format and have .yml or .yaml extension" required:"true"`
+}
+
+func (command *ArgsCommandConfigCommandValidate) Execute(args []string) error {
+	if command.Debug {
+		log.SetLevel(log.DebugLevel)
+	} else {
+		log.SetLevel(log.WarnLevel)
+	}
+	_, err := config.Load(command.ConfigFile, command.Debug, &sync.Mutex{})
+	if err != nil{
+		fmt.Printf("Config file %s did not pass validation\n", command.ConfigFile)
+		os.Exit(1)
+	} else {
+		fmt.Printf("Config file %s is valid\n", command.ConfigFile)
+		os.Exit(0)
+	}
+	return nil
+}
+
+func (command *ArgsCommandConfigCommandDump) Execute(args []string) error {
+	if command.Debug {
+		log.SetLevel(log.DebugLevel)
+	} else {
+		log.SetLevel(log.WarnLevel)
+	}
+	configuration, err := config.Load(command.ConfigFile, command.Debug, &sync.Mutex{})
+	if err == nil {
+		utils.Pp(configuration.GetWithLock(loggingContext))
+		os.Exit(0)
+	} else {
+		fmt.Printf("Config file %s did not pass validation\n", command.ConfigFile)
+		os.Exit(1)
+	}
+	return nil
+}
+
+func (command *ArgsCommandStart) Execute(args []string) error {
+	loggingArgs := misc.LoggingArgs{
+		Verbose: command.Verbose,
+		Debug: command.Debug,
+		TextLog: command.TextLog,
+	}
+	misc.SetupLogging(loggingArgs)
+	daemon.Start(command.ConfigFile, command.Debug)
+	return nil
+}
