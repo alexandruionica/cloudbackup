cd46a0b076ac0afdb573c455eb2c394a591e02f8
Alexandru Ionica <alexandru@ionica.eu>
2019-05-11 20:38:48 +1000

    fixed various tests to accompany the previous code changed which added locking to DB opening

--- shared/structs_dbops.go
+++ shared/structs_dbops.go
@@ -4,7 +4,9 @@ import "database/sql"
 
 type DbData struct {
 	Db *sql.DB
-	// db name, used only for error messages
+	// it is CRITICAL that the name matches the name of the backup job as defined in the configuration file.
+	// If not, it will end up with a null pointer dereference caused by another function which reads DbData.Name and
+	// uses it to search a map containing pointers
 	Name               string
 	Connected          bool
 	PreparedStatements DbPreparedStatements
